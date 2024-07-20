96669f2284ceab1d56dc9a61c74147d08797469d
Jonathan Quinn <1213631+jnq@users.noreply.github.com>
2024-07-20 16:21:54 +0100

    some sytanx improvement

--- get_strava_activities.py
+++ get_strava_activities.py
@@ -1,15 +1,14 @@
 import os, json, time
 from stravalib.client import Client
 
-client = Client()
-
-MY_STRAVA_CLIENT_ID = os.environ.get('MY_STRAVA_CLIENT_ID')
-MY_STRAVA_CLIENT_SECRET = os.environ.get('MY_STRAVA_CLIENT_SECRET')
-MY_STRAVA_CODE = os.environ.get('MY_STRAVA_CODE')
+global CLIENT
+global MY_STRAVA_CLIENT_ID
+global MY_STRAVA_CLIENT_SECRET
+global MY_STRAVA_CODE
 
 def get_auth():
     if MY_STRAVA_CODE:
-        access_token = client.exchange_code_for_token(
+        access_token = CLIENT.exchange_code_for_token(
             client_id=MY_STRAVA_CLIENT_ID,
             client_secret=MY_STRAVA_CLIENT_SECRET,
             code=MY_STRAVA_CODE
@@ -17,7 +16,7 @@ def get_auth():
         with open('access_token', 'w') as f:
             f.write(json.dumps(access_token))
     else:
-        url = client.authorization_url(
+        url = CLIENT.authorization_url(
             client_id=MY_STRAVA_CLIENT_ID,
             redirect_uri='http://localhost:5000/authorization',
             scope=['read_all', 'activity:read_all', 'profile:read_all']
@@ -28,33 +27,37 @@ def check_auth():
     token = open('access_token', 'r').read()
     token = json.loads(token)
     if time.time() > token['expires_at']:
-        refresh = client.refresh_access_token(
+        refresh = CLIENT.refresh_access_token(
                     client_id=MY_STRAVA_CLIENT_ID,
                     client_secret=MY_STRAVA_CLIENT_SECRET,
                     refresh_token=token['refresh_token']
                 )
     else:
         refresh = False
-        client.access_token = token['access_token']
-        client.refresh_token = token['refresh_token']
-        client.token_expires_at = token['expires_at']
+        CLIENT.access_token = token['access_token']
+        CLIENT.refresh_token = token['refresh_token']
+        CLIENT.token_expires_at = token['expires_at']
         print('Access token is still valid')
     if refresh:
-        client.access_token = refresh['access_token']
-        client.refresh_token = refresh['refresh_token']
-        client.token_expires_at = refresh['expires_at']
+        CLIENT.access_token = refresh['access_token']
+        CLIENT.refresh_token = refresh['refresh_token']
+        CLIENT.token_expires_at = refresh['expires_at']
         with open('access_token', 'w') as f:
             f.write(json.dumps(refresh))
 
 
 if __name__ == '__main__':
+    CLIENT = Client()
+    MY_STRAVA_CLIENT_ID = os.environ.get('MY_STRAVA_CLIENT_ID')
+    MY_STRAVA_CLIENT_SECRET = os.environ.get('MY_STRAVA_CLIENT_SECRET')
+    MY_STRAVA_CODE = os.environ.get('MY_STRAVA_CODE')
     try:
         with open('access_token', 'r') as f:
             access_token = json.loads(f.read())
-            client.access_token = access_token
+            CLIENT.access_token = access_token
             check_auth()
     except FileNotFoundError:
         get_auth()
 
-    athlete = client.get_athlete()
+    athlete = CLIENT.get_athlete()
     print(f'Hello, {athlete.firstname} {athlete.lastname}!')
